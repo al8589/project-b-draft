fe1d8f2a8c09f63f359ef83b4148caf9004d73a9
al8589 <147701364+al8589@users.noreply.github.com>
2023-12-01 15:47:31 +0800

    hhhh

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var rain = [];
 var speed;
 
+let mySound;
+
+function preload() {
+  mySound = loadSound("lib/sound/forestrain.wav");
+}
+
 function setup() {
-  createCanvas(600, 600);
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('canvasWrapper');
   for (var i = 0; i < 800; i++) {
     rain[i] = new Rain();
   }
+  mySound.play();
 }
 
 function draw() {
+  if(!mySound.isPlaying()){
+
+    mySound.loop();
+  }
   clear();
   var distance = dist(mouseX, mouseY, width / 2, height / 2);
-  speed = map(distance, 0, dist(0, 0, width / 2, height / 2), 30, 0);
-   
+  speed = map(distance, 0, dist(0, 0, width / 2, height / 2), 35, 0);
+  
   translate(width / 2, height / 2);
   for (var i = 0; i < rain.length; i++) {
     rain[i].update();
